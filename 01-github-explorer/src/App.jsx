3b3433784e4261b0b31945eac267bcde276aeb68
wakenedo <a_nuner@outlook.com>
2021-11-08 12:26:00 -0300

    Source Map

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,3 +1,5 @@
 export function App() {
+    throw new Error('Eita Caiu, pai!')
+
    return <h1> Hello React.js </h1> 
 }
--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
+    devtool: 'eval-source-map', 
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -12,7 +13,9 @@ module.exports = {
         extensions: ['.js', '.jsx'],
     },
     devServer: {
-        contentBase: path.resolve(__dirname, 'public')
+        static: {
+            directory: path.resolve(__dirname, 'public')
+        },      
     },
     plugins: [
         new HtmlWebpackPlugin({
